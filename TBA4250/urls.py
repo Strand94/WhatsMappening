bcf4f7f1b76260a2d47ff9608e2b388b983faa9e
Strand94 <andreasstrand94@gmail.com>
2018-02-17 21:11:15 +0100

    Users can now upload profile image

--- TBA4250/urls.py
+++ TBA4250/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -33,4 +35,5 @@ urlpatterns = [
     url(r'^settings/password/$', views.password, name='password'),
     url(r'^settings/user/$', views.update_profile, name='update_profile'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
