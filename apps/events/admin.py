2532369345d6d1939c95b68696614e854e18673e
Strand94 <andreasstrand94@gmail.com>
2018-03-07 23:50:28 +0100

    Making changes to the admin interface, and giving models a defined name.

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
-from . models import *
 
-# Register your models here.
-admin.site.register(Event)
-admin.site.register(Attendance)
-admin.site.register(Participation)
+from .models import *
+
+
+class ParticipationInline(admin.TabularInline):
+    model = Participation
+
+
+class AttendanceAdmin(admin.ModelAdmin):
+    model = Attendance
+    filter_horizontal = ('participants',)
+    inlines = [
+        ParticipationInline,
+    ]
+
+
+class AttendanceInline(admin.StackedInline):
+    model = Attendance
+    filter_horizontal = ('participants',)
+    extra = 0
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        AttendanceInline,
+    ]
+    exclude = ('timestamp','author')
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
 admin.site.register(Category)
