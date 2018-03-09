c6e69bca6a9d7c2f1bd03116f531b1e77459f123
Strand94 <andreasstrand94@gmail.com>
2018-03-09 13:04:03 +0100

    The date and time selectors now uses the date time selector widgets supported in the admin section. #8

--- apps/events/views.py
+++ apps/events/views.py
@@ -17,6 +17,8 @@ def create_event(request):
             event = form.save(commit=False)
             event.author = request.user
             event.timestamp = timezone.now()
+            event.start = str(request.POST.get(("startDate"), "")+" "+request.POST.get(("startTime"), ""))
+            event.end = str(request.POST.get(("endDate"), "")+" "+request.POST.get(("endTime"), ""))
             event.save()
             attendance = Attendance.objects.create(event=event)
             participation = Participation.objects.create(attendance=attendance, user=request.user, status='G')
@@ -32,18 +34,27 @@ def create_event(request):
 
 def edit_event(request, pk):
     event = get_object_or_404(Event, pk=pk)
+    start_dateTime = str(event.start).split()
+    end_dateTime = str(event.end).split()
+    start_date = (start_dateTime[0])
+    start_time = (start_dateTime[1][0:8])
+    end_date = (end_dateTime[0])
+    end_time = (end_dateTime[1][0:8])
     owner = event.author
     action = 'Edit'
     if request.method == 'POST':
         form = EventForm(request.POST, instance=event)
+        print("load form")
         if form.is_valid():
             event = form.save(commit=False)
             event.author = request.user
             event.timestamp = timezone.now()
+            event.start = str(request.POST.get(("startDate"), "") + " " + request.POST.get(("startTime"), ""))
+            event.end = str(request.POST.get(("endDate"), "") + " " + request.POST.get(("endTime"), ""))
             event.save()
             return redirect('events:event_detail', pk=event.pk)
     else:
-        form = EventForm(instance=event)
+        form = EventForm(instance=event, initial={'startDate': start_date, 'startTime': start_time, 'endDate': end_date, 'endTime': end_time})
     return render(request, 'events/eventForm.html',{
         'form':form,
         'action':action,
