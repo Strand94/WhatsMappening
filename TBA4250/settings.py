d58b27c17a913ac4dcfe5245383a903d3f03c07c
Strand94 <Andreas@anstra.no>
2018-04-23 13:10:31 +0200

    Fixing small deployment issues - adding GDAL and geos with version to requiremens.txt - updating settings file for heroku

--- TBA4250/settings.py
+++ TBA4250/settings.py
@@ -172,6 +172,11 @@ MEDIA_URL = '/uploads/'
 MEDIA_ROOT = '/uploads/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 
 STATICFILES_DIRS = [
