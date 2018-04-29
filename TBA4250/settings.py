0b733791f1b7a625b218364266819c8ea19a1d34
thaiKari <karimelingjohannessen@gmail.com>
2018-04-29 13:42:39 +0200

    Added search icon to createEvent map

--- TBA4250/settings.py
+++ TBA4250/settings.py
@@ -114,8 +114,6 @@ SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
     'fields': 'id,name,email,gender',
 }
 
-
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -173,18 +171,22 @@ MEDIA_ROOT = '/uploads/'
 
 STATIC_URL = '/static/'
 
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (63.4305, 10.3951),
     'DEFAULT_ZOOM': 10,
-    'ATTRIBUTION_PREFIX': 'whatsmappening'
+    'ATTRIBUTION_PREFIX': 'whatsmappening',
+    'PLUGINS': {
+        'leaflet-search': {
+            'css': STATIC_URL + 'css/leaflet-search.css',
+            'js': STATIC_URL + 'js/leaflet-search.js',
+            'auto-include': True,
+        },
+        'forms': {
+            'auto-include': True
+        }
+    }
 }
-
-
-
-
